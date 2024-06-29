cc5b5a0a3be7c4b5e4c2e86143fa84c39d52d0b4
saravanan <saravanan12e2@gmail.com>
2024-06-29 12:39:19 +0530

    MVC architecture

---
+++ src/main/java/controller/MVCServlet.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.catalina.connector.Response;
+
+import model.MVCJAVA1;
+
+@WebServlet("/MVCServlet")
+public class MVCServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		int N1=Integer.parseInt(req.getParameter("num1"));
+		MVCJAVA1 ob=new MVCJAVA1();
+		boolean check=ob.disp(N1);
+		String ans="ss";
+		if(check) {
+			ans="Palindrome";
+			
+		}
+		else {
+			ans="Not Palindrome";
+		}
+	HttpSession sc=req.getSession();
+	sc.setAttribute("value",ans);
+	RequestDispatcher dp=req.getRequestDispatcher("index.jsp");
+	dp.forward(req, res);
+	}
+
+}
---
+++ src/main/java/model/MVCJAVA1.java
@@ -0,0 +1,15 @@
+package model;
+
+public class MVCJAVA1 {
+     public  boolean disp(int n) {
+         int a=n;
+         int n2=0;
+         while(a!=0) {
+        	 int mod=a%10;
+        	 n2=n2*10+mod;
+        	 a/=10;
+         }
+         if(n-n2==0) return true;
+         else return false;
+}
+}
